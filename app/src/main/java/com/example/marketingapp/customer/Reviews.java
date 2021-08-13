9cd9bbc338540add07e46b445a7b0fa8bda3da69
Sandeep-0404 <sandeepgh2002@gmail.com>
2021-08-13 18:09:04 +0530

    add review completed

--- app/src/main/java/com/example/marketingapp/customer/Reviews.java
+++ app/src/main/java/com/example/marketingapp/customer/Reviews.java
@@ -1,20 +1,22 @@
 package com.example.marketingapp.customer;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-import com.example.marketingapp.R;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 import com.example.marketingapp.databinding.FragmentReviewsBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.zip.Inflater;
 
 
 public class Reviews extends Fragment {
@@ -26,38 +28,61 @@ public class Reviews extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-       binding=FragmentReviewsBinding.inflate(inflater,container,false);
+        binding = FragmentReviewsBinding.inflate(inflater, container, false);
 
-       binding.recReview.setLayoutManager(new LinearLayoutManager(getContext()));
-       data=new ArrayList<>();
-       review_model m=new review_model();
-       m.setBody("body");
-       m.setDate("date");
-       m.setUsername("username");
-       m.setOrderid("orderid");
+        binding.recReview.setLayoutManager(new LinearLayoutManager(getContext()));
+        data = new ArrayList<>();
 
-       data.add(m);
-        reviewAdapter=new review_adapter(data,getContext());
+        reviewAdapter = new review_adapter(data, getContext());
 
-       binding.recReview.setAdapter(reviewAdapter);
+        binding.recReview.setAdapter(reviewAdapter);
+        getData();
 
-       binding.addReview.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
+        binding.addReview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-               addReview();
+                addReview();
 
-           }
-       });
-
+            }
+        });
 
 
         return binding.getRoot();
     }
 
+    private void getData() {
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("CustomerReviews");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                data.clear();
+                for (DataSnapshot s : snapshot.getChildren()) {
+
+                    review_model md = s.getValue(review_model.class);
+                    data.add(md);
+
+
+                }
+                reviewAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+    }
+
     private void addReview() {
 
+        addReviewCustomer addReviewCustomer = new addReviewCustomer();
 
+        addReviewCustomer.show(getChildFragmentManager(), addReviewCustomer.getTag());
 
 
     }
