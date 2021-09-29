e9faa9ad1974ba0d8d25e3363441fd601ae0bdf8
anisha-01-phoenix <anisha.dat01@gmail.com>
2021-09-29 21:50:28 +0530

    XML updated

--- app/src/main/java/com/example/marketingapp/shopkeeper/add_items_activity.java
+++ app/src/main/java/com/example/marketingapp/shopkeeper/add_items_activity.java
@@ -49,13 +49,13 @@ public class add_items_activity extends AppCompatActivity {
     Uri filepath;
     Bitmap bitmap;
     StorageReference storageReference;
-    String photoUrl="";
+    String photoUrl = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       binding=ActivityAddItemsBinding.inflate(getLayoutInflater());
-       setContentView(binding.getRoot());
+        binding = ActivityAddItemsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
 
         String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
@@ -88,7 +88,7 @@ public class add_items_activity extends AppCompatActivity {
         });
 
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Shopkeeper");
-        storageReference= FirebaseStorage.getInstance().getReference();
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         binding.uploadNow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,9 +104,9 @@ public class add_items_activity extends AppCompatActivity {
                         uploader.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                             @Override
                             public void onSuccess(Uri uri) {
-                                photoUrl=uri.toString();
+                                photoUrl = uri.toString();
                                 binding.uploadNow.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_task_alt_24));
-
+                                binding.addprodimg.setText("Image Added!");
                                 progressDialog.dismiss();
                             }
                         });
@@ -145,11 +145,11 @@ public class add_items_activity extends AppCompatActivity {
                     model_shopcontent model = new model_shopcontent(i, shopId, photoUrl,
                             binding.prodnamefrag.getEditText().getText().toString(),
                             binding.prodqtavailable.getEditText().getText().toString(),
-                            binding.proddesc.getEditText().getText().toString(), "Rs. "+
+                            binding.proddesc.getEditText().getText().toString(), "Rs. " +
                             binding.prodprice.getEditText().getText().toString(),
-                            binding.prodofferprice.getEditText().getText().toString()+"%");
+                            binding.prodofferprice.getEditText().getText().toString() + "%");
                     ref1.child(i).setValue(model);
-                    Toasty.normal(getApplicationContext(),"Product Image will be Updated Soon...").show();
+                    Toasty.normal(getApplicationContext(), "Product Image will be Updated Soon...").show();
 
                 }
             }
@@ -158,16 +158,14 @@ public class add_items_activity extends AppCompatActivity {
     }
 
     private boolean isChk() {
-        if (photoUrl.equalsIgnoreCase(""))
-        {
-            Toasty.error(getApplicationContext(),"Please add an image of Your Product").show();
+        if (photoUrl.equalsIgnoreCase("")) {
+            Toasty.error(getApplicationContext(), "Please add an image of Your Product").show();
             return false;
         }
         if (binding.prodnamefrag.getEditText().getText().toString().isEmpty() || binding.prodqtavailable.getEditText().getText().toString().isEmpty()
                 || binding.proddesc.getEditText().getText().toString().isEmpty() || binding.prodprice.getEditText().getText().toString().isEmpty() ||
-                binding.prodofferprice.getEditText().getText().toString().isEmpty())
-        {
-            Toasty.error(getApplicationContext(),"Please enter all the fields").show();
+                binding.prodofferprice.getEditText().getText().toString().isEmpty()) {
+            Toasty.error(getApplicationContext(), "Please enter all the fields").show();
             return false;
         }
         return true;
@@ -203,4 +201,13 @@ public class add_items_activity extends AppCompatActivity {
     }
 
 
+    public void bcktoShop(View view) {
+        onBackPressed();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(add_items_activity.this, shopkeepercontent.class));
+    }
 }
